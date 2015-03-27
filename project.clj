1b6adcd181d8cb399c2eff39f15d5ae6ed77bee5
Marcos <marcoslamuria@gmail.com>
2015-03-27 23:56:16 +0000

    add travis

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject email-validator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject email-validator "0.1"
+  :description "validate emails"
+  :url "https://github.com/lamuria/email-validator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
+  :aliases {"all" ["with-profile" "dev:1.5:1.7" "test"]})
