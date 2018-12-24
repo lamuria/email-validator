3d594788321f24faa3d33bcb0b6874b325b11e17
hendrik <hennie.g.louw@gmail.com>
2018-12-24 17:10:21 +1100

    - Fixed up travis file since last time it was used. - Removed version 1.5 & 1.7 from build.

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
-  :aliases {"all" ["with-profile" "dev:1.5:1.7" "test"]})
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :aliases {"all" ["with-profile" "dev:1.9" "test"]})
